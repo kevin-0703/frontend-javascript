ab094db6b0554d7b2eab89aa4e5652a72cbb9540
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-09 18:15:32 +0200

    creating a Student interface

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,24 +1,24 @@
-interface student {
+interface Student {
   firstName: string;
   lastName: string;
   age: number;
   location: string;
 }
 
-const student1: student = {
+const student1: Student = {
   firstName: "John",
   lastName: "Doe",
   age: 20,
   location: "New York",
 };
-const student2: student = {
+const student2: Student = {
   firstName: "Jane",
   lastName: "Smith",
   age: 22,
   location: "Los Angeles",
 };
 
-const studentsList: student[] = [student1, student2];
+const studentsList: Student[] = [student1, student2];
 
 const table = document.createElement("table");
 const tbody = document.createElement("tbody");
